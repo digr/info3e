827e8d4a7885b38b6a2a60236da3559b2cfbd32e
Dmitry Grushin <dgrushin@gmail.com>
2020-04-23 12:28:48 +0300

    Show self assessment only when available for cources

--- app/controllers/assessments_controller.rb
+++ app/controllers/assessments_controller.rb
@@ -2,11 +2,13 @@ class AssessmentsController < ApplicationController
     def create
         user_id = current_user_info.id
 
-        params[:assessments].each do |cource, value|
-            unless value.blank?
-                assmnt = SelfAssessment.find_or_create_by(user_id: user_id, cource_id: cource, date: date)
-                assmnt.value = value.to_i
-                assmnt.save!
+        if params[:assessments]
+            params[:assessments].each do |cource, value|
+                unless value.blank?
+                    assmnt = SelfAssessment.find_or_create_by(user_id: user_id, cource_id: cource, date: date)
+                    assmnt.value = value.to_i
+                    assmnt.save!
+                end
             end
         end
         redirect_to :root
